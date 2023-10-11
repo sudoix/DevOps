96811c03dea9cbf1ba7dee30e14fcc90131e3a17
Sudoix <sudoix.com@gmail.com>
2023-10-11 10:29:41 +0330

    add script file

---
+++ linux/scripts/00.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo hello world
+echo hello world!
+# Run in terminal
+echo hello world!!
+echo hello world!!!!!!! # :)
+
+# For print !!!! in normal mode
+echo hello world\!\!\!\!
+
+echo -e "hello \nworld" # Use ""
+echo -e "hello \tworld" # Use ""
+echo -e "hello \nworld" # Run in sh
+echo -e hello \nworld
+
+# Redirect and append
+echo -e "hello world" > file.txt
+echo -e "hello world" >> file.txt
+
+# Read file
+cat file.txt
+
+
+
+
+
---
+++ linux/scripts/01.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "enter your name: "
+read name
+echo "your name is $name"
+
+read -p "enter your name: " name
+echo "your name is $name"
+
+
